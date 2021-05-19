0bc603e40f19cac0001b2964c452a98c517bc054
Abderahmane Aoufi <abdouou7@gmail.com>
2021-05-19 01:09:48 +0100

    Optimized sorting

--- Javascript/Sorting Algorithms/Bubble Sort/Bubble.js
+++ Javascript/Sorting Algorithms/Bubble Sort/Bubble.js
@@ -1,4 +1,8 @@
 // simple function show how bubble sort works
+// ! Bubble sort is not the best sorting algorithms so it's less efficient !
+// ? Asending => smallest to greatest
+// largest value sinking to the end
+// ? Big(O) => o(n^2)
 function bubbleSort(arr) {
   let right = 0;
   let left = 0;
@@ -8,10 +12,10 @@ function bubbleSort(arr) {
     for (let i = 1; i <= arr.length - 1; i++) {
       right = arr[i];
       left = arr[i - 1];
+      console.log(arr, arr[i], arr[i - 1]);
       if (right < left) {
         // we have to swap here !!
-        arr[i] = left;
-        arr[i - 1] = right;
+        [arr[i], arr[i - 1]] = [arr[i - 1], arr[i]];
         // this will assure the next loop !!
         swap = true;
       }
@@ -21,6 +25,40 @@ function bubbleSort(arr) {
 }
 
 const unsortedArray = [1, 3, 2, 6, 4, 12, 11, 10, 9, 7];
-const sortedArray = bubbleSort(unsortedArray); // before => [ 1, 3, 2, 6, 4, 12, 11, 10, 9, 7 ] 
-console.log(sortedArray); // result => [ 1, 2, 3, 4, 6, 7, 9, 10, 11, 12 ]
- 
+// const sortedArray = bubbleSort(unsortedArray); // before => [ 1, 3, 2, 6, 4, 12, 11, 10, 9, 7 ]
+// console.log(sortedArray); // result => [ 1, 2, 3, 4, 6, 7, 9, 10, 11, 12 ]
+
+// Old way
+function swap(arr, index1, index2) {
+  let temp = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = temp;
+}
+
+// ES2015
+const swap2 = (arr, index1, index2) => {
+  [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
+};
+
+// implementing bubbleSort
+function betterBubleSort(arr) {
+  let swap = true;
+  let count = 0 ;
+  for (let i = arr.length; i > 0; i--) {
+    if (swap) {
+      swap = false ;
+      count ++ ;
+      for (let j = 0; j < i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          swap = true ;
+          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        }
+      }
+    }
+  }
+  return [arr , count];
+}
+
+const unsortedArray2 = [2 , 3 , 1  ];
+const sortedArrayBetter = betterBubleSort(unsortedArray2);
+console.log(sortedArrayBetter);
--- Javascript/Sorting Algorithms/Built_InJsSorting/Sort.js
+++ Javascript/Sorting Algorithms/Built_InJsSorting/Sort.js
@@ -9,8 +9,26 @@ const students = [
   { name: "Wassim", score: 10 },
 ];
 
-const sortedArray = students.sort(function (studentA, studentB) {
-  return studentA.score - studentB.score;
-}).reverse();
+const sortedArray = students
+  .sort(function (studentA, studentB) {
+    return studentA.score - studentB.score;
+  })
+  .reverse();
 
 console.log(sortedArray);
+
+const sortedByNames = students.sort(function (studentA, studentB) {
+  var nameA = studentA.name.toUpperCase(); // ignore upper and lowercase
+  var nameB = studentB.name.toUpperCase(); // ignore upper and lowercase
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+
+  // names must be equal
+  return 0;
+});
+
+console.log(sortedByNames)
