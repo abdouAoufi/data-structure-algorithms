b2994ebcb5427b2534c640a8dcb6eea69d4b8128
Abderahmane Aoufi <abdouou7@gmail.com>
2021-06-15 17:17:26 +0100

    add queue

--- Javascript/Data structure/Queue/Queue.js
+++ Javascript/Data structure/Queue/Queue.js
@@ -15,9 +15,38 @@ class Queue {
     const node = new Node(val);
     if (!this.first) {
       this.first = node;
+    } else {
+      this.last.next = node;
     }
     this.last = node;
-    this.size ++ ;
+    return ++this.size;
   }
-  deQueue() {}
+  deQueue() {
+    if (!this.first) return null;
+    let temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.val;
+  }
+  print(){
+   let elem = this.first;
+   let counter = 0;
+   while (counter < this.size) {
+     console.log(elem.val);
+     elem = elem.next;
+     counter++;
+   }
+  }
 }
+
+
+let q = new Queue();
+q.enQueue(1);
+q.enQueue(2);
+q.enQueue(3);
+q.enQueue(4);
+console.log(q.deQueue());
+// q.print();
--- Javascript/Data structure/Stack/Stack.js
+++ Javascript/Data structure/Stack/Stack.js
@@ -49,5 +49,4 @@ stack.push(1);
 stack.push(2);
 stack.push(3);
 stack.push(4);
-stack.pop();
 stack.print();
