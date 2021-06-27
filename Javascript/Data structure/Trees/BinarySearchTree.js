7045a09d715a26730fab1ac302444fd68baad712
Abderahmane Aoufi <abdouou7@gmail.com>
2021-06-27 02:48:02 +0100

    add find

--- Javascript/Data structure/Trees/BinarySearchTree.js
+++ Javascript/Data structure/Trees/BinarySearchTree.js
@@ -1,55 +1,70 @@
 class Node {
-  constructor(value){
-      this.value = value;
-      this.left = null;
-      this.right = null;
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
 }
 
 class BinarySearchTree {
-  constructor(){
-      this.root = null;
+  constructor() {
+    this.root = null;
   }
-  insert(value){
-      var newNode = new Node(value);
-      if(this.root === null){
-          this.root = newNode;
+  insert(value) {
+    var newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    var current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
           return this;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
       }
-      var current = this.root;
-      while(true){
-          if(value === current.value) return undefined;
-          if(value < current.value){
-              if(current.left === null){
-                  current.left = newNode;
-                  return this;
-              }
-              current = current.left;
-          } else {
-              if(current.right === null){
-                  current.right = newNode;
-                  return this;
-              } 
-              current = current.right;
-          }
+    }
+  }
+  find(value) {
+    if (this.root === null) {
+      return false;
+    }
+    var current = this.root;
+    var found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
       }
+    }
+    if (!found) return false;
+    return true;
   }
 }
 
-
 //      10
-//   5     13
-// 2  7  11  16
+//   5   |    13
+// 2 | 7  | 11 | 16
 
 var tree = new BinarySearchTree();
-tree.insert(10)
-tree.insert(5)
-tree.insert(13)
-tree.insert(11)
-tree.insert(2)
-tree.insert(16)
-tree.insert(7)
-
-
-
-
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
+var result = tree.find(7);
+console.log(result);
